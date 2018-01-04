02120612271ba3a6d894696293be54fcf2fd6892
Jindrich Durcak <jindrich.durcak@merck.com>
2018-01-04 14:24:57 +0100

    package creation with Roxygen

---
+++ ch11BuildingRpackage/R/document_test.R
@@ -0,0 +1,10 @@
+#' Add two numbers
+#'
+#' This function adds two numbers together.
+#'
+#' @param x  A number
+#' @param y  Another number
+#' @return The sum of x and y
+#'
+#' @export
+add <- function(x, y) x + y
